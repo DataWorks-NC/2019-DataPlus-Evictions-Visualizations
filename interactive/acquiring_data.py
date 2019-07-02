731c767bb6c4e5a690a2a4ef4a2828e83d97121e
ellisacker <39772766+ellisacker@users.noreply.github.com>
2019-07-02 15:09:40 -0400

    Added files
    Updated piplock, added acquiring data python file.

---
+++ interactive/acquiring_data.py
@@ -0,0 +1,47 @@
+#acquiring data
+#imports
+from datetime import datetime
+startTime = datetime.now() 
+import sys, os
+import numpy as np
+import pandas as pd
+import pandas.io.sql as psql
+import psycopg2
+import _functions as fn
+#---------------------------------------------------------------#
+#user input of credentials
+name=input('Username: ')
+pssword=input('Password: ')
+#---------------------------------------------------------------#
+PGHOST='dataworksnc.cvjygj6lx0j8.us-east-1.rds.amazonaws.com'
+PGDATABASE='dataworksnc'
+PGUSER=name
+PGPASSWORD=pssword
+#---------------------------------------------------------------#
+#set up a connection to the postgres server.
+conn_string = 'host='+ PGHOST +' port='+ '5432' +' dbname='+ PGDATABASE +' user=' + PGUSER +' password='+ PGPASSWORD
+conn=psycopg2.connect(conn_string)
+print('Connected to Server')
+#create a cursor object
+cursor = conn.cursor()
+#---------------------------------------------------------------#
+#test eviction data loading
+evictions=fn.load_data('housing','evictions')
+summary_ejectments_blockgroup_year=fn.load_data('housing','summary_ejectments__blockgroup__year')
+summary_ejectments_tract_year=fn.load_data('housing','summary_ejectments__tract__year.')
+#evictions_blockgroup_months.csv# to add
+#evictions_tract_months.csv# to add
+#evictionslatlong.csv# to add
+print('Data Loaded From: '+PGDATABASE)
+print('Time to Load: ',datetime.now() - startTime)
+#---------------------------------------------------------------#
+#save data to location
+evictions.to_csv('./data/dataworksnc_housing_evictions.csv')
+summary_ejectments_blockgroup_year.to_csv('./data/dataworksnc_housing_summary_ejectments__blockgroup__year.csv')
+summary_ejectments_tract_year.to_csv('./data/dataworksnc_housing_summary_ejectments__tract__year.csv')
+## ADD.to_csv('./data/evictions_blockgroup_months.csv')
+## ADD.to_csv('.data/evictions_tract_months.csv')
+## ADD.to_csv('.data/evictionslatlong.csv')
+print('data saved to working directory')
+#---------------------------------------------------------------#
+print('time to run: ',datetime.now() - startTime)
