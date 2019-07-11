bb3520a59857c401f9f1b9fa360afa56f9d0a9f7
Ellis A <39772766+ellisacker@users.noreply.github.com>
2019-07-11 14:35:05 -0400

    normframe?

--- interactive/heatmap.py
+++ interactive/heatmap.py
@@ -13,15 +13,26 @@ from bokeh.tile_providers import get_provider, Vendors, CARTODBPOSITRON
 from functions import get_base_dir
 #################################################################
 # Load Pickles Dataframesdf = pd.read_pickle(f'{get_base_dir()}/pickled_files/df.pkl')
-df_max_min = pd.read_pickle(f'{get_base_dir()}/pickled_files/maxmin.pkl')
-df_dates = pd.read_pickle(f'{get_base_dir()}/pickled_files/sorted.pkl')
-sorted_unique_dates = df_dates.sorted_unique_dates.tolist()
-initial = pd.read_pickle(f'{get_base_dir()}/pickled_files/initial.pkl')
+# df_max_min = pd.read_pickle(f'{get_base_dir()}/pickled_files/maxmin.pkl')
+# df_dates = pd.read_pickle(f'{get_base_dir()}/pickled_files/sorted.pkl')
+# sorted_unique_dates = df_dates.sorted_unique_dates.tolist()
+# initial = pd.read_pickle(f'{get_base_dir()}/pickled_files/initial.pkl')
+
+# normframe=[]
+# for i in range(0,len(sorted_unique_dates)):
+#   df = pd.read_pickle(f'{get_base_dir()}/pickled_files/jar/KDE_' + str(i) + '.pkl')
+#   normframe.append(df['KDE'].tolist()[0])
+df=pd.read_pickle(f'{get_base_dir()}/pickled_files/df.pkl')
+df_max_min=pd.read_pickle(f'{get_base_dir()}/pickled_files/maxmin.pkl')
+
+dates=df['date'].tolist()
+unique_dates=list(set(dates))
+sorted_unique_dates=sorted(unique_dates)
 
 normframe=[]
 for i in range(0,len(sorted_unique_dates)):
-  df = pd.read_pickle(f'{get_base_dir()}/pickled_files/jar/KDE_' + str(i) + '.pkl')
-  normframe.append(df['KDE'].tolist()[0])
+    t = pd.read_pickle(f'{get_base_dir()}/pickled_files/jar/KDE_' + str(i) + '.pkl')
+    normframe.append(t['KDE'].tolist()[0])
 #---------------------------------------------------------------#
 # ColumnDataSource Setup
 source = ColumnDataSource(data=dict(image=[normframe[0]], x=df_max_min.xmin, y=df_max_min.ymin))
