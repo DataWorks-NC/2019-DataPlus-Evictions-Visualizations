d254ffc3510c34ab061a322c80ab87b0f209136d
Rodrigo Araujo <42783261+RodrigoAra@users.noreply.github.com>
2019-07-16 11:05:45 -0400

    Update heat_data.py

--- interactive/heat_data.py
+++ interactive/heat_data.py
@@ -14,7 +14,7 @@ data_path= f'{fn.get_base_dir()}/data/evictionslatlong.csv'
 bins = 1000
 tolerance = 0.01   #for KDEs
 bw_divider = 1.5   #what to divide bandwidth by to show neighborhoods more clearly
-#################################################################
+#---------------------------------------------------------------#
 #load data 
 df = pd.read_csv(data_path)
 df=df[df['process']=='Summary Ejectment']
@@ -22,32 +22,28 @@ df['date']=df['status_date'].apply(fn.time)
 df=df.sort_values('date')
 df['x']=df['x'].apply(fn.reflect)
 #globals
-xy=[df['x'].tolist(), df['y'].tolist()]
-X=xy[0]
-Y=xy[1]
-xmin=min(X)
-xmax=max(X)
-ymin=min(Y)
-ymax=max(Y)
+X=np.array(df['x'].tolist())
+Y=np.array(df['y'].tolist())
 #---------------------------------------------------------------#
 #unique dates to analyze
 dates=df['date'].tolist()
 unique_dates=list(set(dates))
 sorted_unique_dates=sorted(unique_dates)
 df_dates = pd.DataFrame({'sorted_unique_dates': sorted_unique_dates})
-df_dates.to_pickle(f'{fn.get_base_dir()}/pickled_files/sorted.pkl')
-#---------------------------------------------------------------#
-#saving max and mins
-df_max_min = pd.DataFrame({'xmin': fn.conv_poly_xs([min(-1 * np.array(xy[0]))]),'xmax': fn.conv_poly_xs([max(-1 * np.array(xy[0]))]),
-                         'ymin': fn.conv_poly_ys([min(xy[1])]),'ymax': fn.conv_poly_ys([max(xy[1])]),'xs': [xy[0]],'ys': [xy[1]]})
-df_max_min.to_pickle(f'{fn.get_base_dir()}/pickled_files/maxmin.pkl')
-#---------------------------------------------------------------#
+#starting date in dataset
 df['xneg']=df['x'].apply(fn.reflect)
 df['XX']=df['xneg'].apply(fn.convX)
 df['YY']=df['y'].apply(fn.convY)
 initial=df[df['date']==sorted_unique_dates[0]]
+#output dataframes to pickles
 initial.to_pickle(f'{fn.get_base_dir()}/pickled_files/initial.pkl')
 df.to_pickle(f'{fn.get_base_dir()}/pickled_files/df.pkl')
+df_dates.to_pickle(f'{fn.get_base_dir()}/pickled_files/sorted.pkl')
+#---------------------------------------------------------------#
+#saving max and mins
+df_max_min = pd.DataFrame({'xmin': fn.conv_poly_xs([min(-1 * X)]),'xmax': fn.conv_poly_xs([max(-1 * X)]),
+                           'ymin': fn.conv_poly_ys([min(Y)]),'ymax': fn.conv_poly_ys([max(Y)]),'xs': [X],'ys': [Y]})
+df_max_min.to_pickle(f'{fn.get_base_dir()}/pickled_files/maxmin.pkl')
 #---------------------------------------------------------------#
 print('--------------------------------------------------------------------------------')
 print('Date Formatting: YEAR.[(MONTH)/100]. For example, July 2019 would be enterted as 2019.07')
@@ -72,7 +68,7 @@ with Bar('Making KDEs', max=barmax,fill='#') as bar:
 			Sbw=(len(X1))**(-1./(2.+4.))
 			bw=Sbw/bw_divider
 
-			A = fn.KDE(X1,Y1,bins,xmin,xmax,ymin,ymax,bw)
+			A = fn.KDE(X1,Y1,bins,min(X),max(X),min(Y),max(Y),bw)
 			density, xxmin, xxmax, yymin, yymax = fn.KDE_plot(A)
 			#normalize and nan it
 			norm = fn.normalize(density)
