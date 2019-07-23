080050b26e236f50031dbadc905a16a241a72d65
Rodrigo Araujo <42783261+RodrigoAra@users.noreply.github.com>
2019-07-23 15:31:16 -0400

    Update main.py

--- interactive/main.py
+++ interactive/main.py
@@ -1,10 +1,10 @@
-import chloropleth
+import choropleth
 import heatmap
 import grid
 from bokeh.io import curdoc
 from bokeh.models.widgets import Tabs, Panel
 
-tab1 = Panel(child=chloropleth.layout, title='Chloropleth')
+tab1 = Panel(child=choropleth.layout, title='Choropleth')
 tab2 = Panel(child=heatmap.layout, title='HeatMap')
 tab3 = Panel(child=grid.layout, title='Calendar')
 tabs = Tabs(tabs=[tab1, tab2, tab3])
