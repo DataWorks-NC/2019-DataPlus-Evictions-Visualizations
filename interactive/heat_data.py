743d561c7702cc15a635ca10da52f29c37941f57
Rodrigo Araujo <rodrigo.araujo@duke.edu>
2019-08-02 01:37:58 -0400

    added heatmap html

--- interactive/heat_data.py
+++ interactive/heat_data.py
@@ -13,7 +13,7 @@ df2 = pd.read_csv(f'{fn.get_base_dir()}/data/evictionslatlong_2000_2011.csv')
 df2['status_date'] = df2['status_date'].apply(fn.datestrconv)
 df = pd.concat([df1,df2])
 #user controled parameters
-bins = 100
+bins = 200
 tolerance = 0.05   #for KDEs
 tolerance2 = 0.25  #for KDE differences 
 bw_divider = 1.5
@@ -35,12 +35,12 @@ missing = [2003.01, 2003.02, 2003.03, 2003.04, 2003.05, 2003.07]
 sorted_unique_dates = sorted(unique_dates + missing)
 
 df_dates = pd.DataFrame({'sorted_unique_dates': sorted_unique_dates})
-df_dates.to_pickle(f'{fn.get_base_dir()}/pickled_files/sorted.pkl')
+# df_dates.to_pickle(f'{fn.get_base_dir()}/pickled_files/sorted.pkl')
 #---------------------------------------------------------------#
 #saving max and mins
 df_max_min = pd.DataFrame({'xmin': fn.conv_poly_xs([min(-1 * X)]),'xmax': fn.conv_poly_xs([max(-1 * X)]),
                            'ymin': fn.conv_poly_ys([min(Y)]),'ymax': fn.conv_poly_ys([max(Y)]),'xs': [X],'ys': [Y]})
-df_max_min.to_pickle(f'{fn.get_base_dir()}/pickled_files/maxmin.pkl')
+# df_max_min.to_pickle(f'{fn.get_base_dir()}/pickled_files/maxmin.pkl')
 #---------------------------------------------------------------#
 print('--------------------------------------------------------------------------------')
 print('Date Formatting: YEAR.[(MONTH)/100]. For example, July 2019 would be enterted as 2019.07')
